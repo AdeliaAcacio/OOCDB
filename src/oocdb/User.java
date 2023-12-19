3980062f45d2e7c261c0fe8fcd69ec492f607484
AdeliaAcacio <2022203@student.cct.ie>
2023-12-19 22:40:11 +0000

    Update User Class
     Update constructor
    Add getter and setter

--- src/oocdb/User.java
+++ src/oocdb/User.java
@@ -6,7 +6,7 @@ package oocdb;
 
 /**
  *
- * @author Stefani Nunes
+ * @author Stefani Nunes & AdeliaAcacio2022203
  */
 
 //User interface
@@ -17,22 +17,61 @@ public void modifyProfile();
 
 public class User {
     
-    String username;
-    String password;
-
-    //CONSTRUCTOR
+    private String userName;
+    private final int userPassword;
+    private final int userID;
+    private final int userAge;
+    protected double userSalary;
     
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+
+    private static final int currentID = 1;
+    
+    //CONSTRUCTOR to buid a new User
+    
+    public User(String userName, int userPassword, int userID, int userAge, double userSalary) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userID = userID;
+        this.userAge = userAge;
+        this.userSalary = userSalary;
     }
+
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getUserPassword() {
+        return UserPassword;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public double getUserSalary() {
+        return userSalary;
+    }
+
+    public static int getCurrentID() {
+        return currentID;
+    }
+
+    public void setUsername(String userName) {
+        this.userName = userName;
+    }
+    
+    
     
     // if the used username and password matches the users choise
-    public boolean login(String userUsername, String userpassword){
-        return this.username.equals(userUsername) && this.password.equals(userpassword);
+    public boolean login(String userUsername, String userPassword){
+        return this.username.equals(userUsername) && this.userPassword.equals(userPassword);
     }
-    
-    
+
  
   
     
