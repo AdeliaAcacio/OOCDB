fae72ceb7a07b7a8a592e4ceb8d5926e100ce216
AdeliaAcacio <2022203@student.cct.ie>
2023-12-19 23:54:29 +0000

    Create TaxType Enum

---
+++ src/oocdb/TaxType.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocdb;
+
+/**
+ * Tax types used to calulate income tax per person in Ireland  
+ * PAYE: 'Pay As You Ern' deducted from employees  
+ * USC: Universal Social Charge
+ * PRSI: Pay Related Social Insurance
+ * 
+ * @author AdeliaAcacio
+ */
+public enum TaxType {
+    PAYE,
+    USC,
+    PRSI
+    
+}
