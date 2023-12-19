baf5074a57a19094be649e4ae56689615957a666
AdeliaAcacio <2022203@student.cct.ie>
2023-12-19 22:37:24 +0000

    Update Prototype class
    ADD methods to run income tax

--- src/oocdb/Prototype_software.java
+++ src/oocdb/Prototype_software.java
@@ -12,6 +12,36 @@ public class Prototype_software {
     
      public static void main(String[] args) {
         // TODO code application logic here  
+        
+         // Retrieve salary and age from the database
+        double salary = retrieveSalaryFromDatabase();
+        int age = retrieveAgeFromDatabase();
+
+        // Create an instance of the DefaultTaxCalculator
+        TaxCalculator taxCalculator = new DefaultTaxCalculator();
+
+        // Calculate taxes using the interface methods
+        double payeTax = taxCalculator.calculatePayeTax(salary);
+        double uscTax = taxCalculator.calculateUscTax(salary, age);
+        double prsiTax = taxCalculator.calculatePrsiTax(salary);
+
+        // Print the results
+        System.out.println("PAYE Tax: " + payeTax);
+        System.out.println("USC Tax: " + uscTax);
+        System.out.println("PRSI Tax: " + prsiTax);
+    }
+
+    // ALTERAR CODIGO
+     // retrieving salary and age from the database
+    private static double retrieveSalaryFromDatabase() {
+        // 
+        return userSalary;
+    }
+
+    private static int retrieveAgeFromDatabase() {
+        // 
+        return age;
+    }
     }
      
 }
